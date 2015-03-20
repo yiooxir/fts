f1c614b871482202c25280d721c8b8af8eacd391
Borovikov Sergey <spectrs@bk.ru>
2015-03-20 18:45:02 +0300

    add send mail notifications

--- models/token.js
+++ models/token.js
@@ -32,10 +32,32 @@ schema.statics.create = function(username, callback) {
         },
         function(a, b, callback) {
             var token = new Token({username: username, token: Math.round(Math.random()*1000000000000)});
-            token.save(function(err, token) {
-                if (err) return callback(err);
+            token.save(callback)
+        },
+        function(token, b, callback) {
+
+            var link = '<p>Сообщение!</p>' +
+                '<a href = http://nsfn.net/auth?token=' +token.token + '>Для регистрации в сервисе перейдите по ссылке</a>';
+
+            var transporter = nodemailer.createTransport({
+                service: 'yandex',
+                auth: {
+                    user: 'nsfn.n@yandex.ru',
+                    pass: 'Nikrol4'
+                }
+            });
+
+            var mailOtptions = {
+                from: 'nsfn.n@yandex.ru',
+                to: 'spectrs@bk.ru',
+                subject: 'Уведомление от сервиса NSFN',
+                html: link
+            };
+
+            transporter.sendMail(mailOtptions, function(err, info) {
+                if (err) return callback(new Error('message was not send' + err));
                 callback(null, token);
-            })
+            });
         }
     ], callback)
 };
